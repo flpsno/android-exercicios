a6e12c910b2f7a096dad566cf6059d583bb559c4
flpsno <flpsno@hotmail.com>
2016-09-07 12:23:59 -0300

    Criado a listagem de clientes usando simple adapter

---
+++ H02_PraticaListView/app/src/main/java/br/com/flpsno/h02_praticalistview/HMAux.java
@@ -0,0 +1,16 @@
+package br.com.flpsno.h02_praticalistview;
+
+import java.util.HashMap;
+
+/**
+ * Created by Felipe on 07/09/2016.
+ */
+public class HMAux extends HashMap<String, String> {
+
+    public static final String ID = "id";
+    public static final String TEXTO_01 = "texto_01";
+    public static final String TEXTO_02 = "texto_02";
+    public static final String TEXTO_03 = "texto_03";
+    public static final String TEXTO_04 = "texto_04";
+    public static final String TEXTO_05 = "texto_05";
+}
--- H02_PraticaListView/app/src/main/java/br/com/flpsno/h02_praticalistview/TelaClientes.java
+++ H02_PraticaListView/app/src/main/java/br/com/flpsno/h02_praticalistview/TelaClientes.java
@@ -1,14 +1,69 @@
 package br.com.flpsno.h02_praticalistview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class TelaClientes extends AppCompatActivity {
+
+    private Context context;
+    //
+    private ListView lv_clientes;
+    //
+    private ArrayList<HMAux> clientes;
+    private SimpleAdapter adapter_clientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.telaclientes);
+        //
+        inicializarVariavel();
+        inicializarAcao();
+    }
+
+    private void inicializarVariavel() {
+        context = getBaseContext();
+        //
+        lv_clientes = (ListView) findViewById(R.id.telaclientes_lv_clientes);
+        //
+        String[] De = {HMAux.TEXTO_01, HMAux.TEXTO_02, HMAux.TEXTO_03, HMAux.TEXTO_04};
+        int[] Para = {R.id.celulaclientes_tv_nome, R.id.celulaclientes_tv_email,
+                R.id.celulaclientes_tv_telefone, R.id.celulaclientes_tv_estado};
+        adapter_clientes = new SimpleAdapter(
+                context,
+                gerarClientes(100),
+                R.layout.celulaclientes,
+                De,
+                Para
+        );
+        lv_clientes.setAdapter(adapter_clientes);
+    }
+
+    private ArrayList<HMAux> gerarClientes(int quantidade) {
+        ArrayList<HMAux> dados = new ArrayList<>();
+        //
+        for (int i = 1; i <= quantidade; i++) {
+            HMAux item = new HMAux();
+            item.put(HMAux.ID, String.valueOf(i));
+            item.put(HMAux.TEXTO_01, "Nome do Cliente - " + String.valueOf(i));
+            item.put(HMAux.TEXTO_02, "clientes" + String.valueOf(i) + "@gmail.com");
+            item.put(HMAux.TEXTO_03, "11 1234-4568");
+            item.put(HMAux.TEXTO_04, "SP");
+            dados.add(item);
+        }
+        //
+        return dados;
+    }
+
+    private void inicializarAcao() {
+
     }
 
     @Override
